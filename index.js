ba5c2223b72c7e4204d8ccaf457325f2eec79a9c
olafontsf <44471831+olafontsf@users.noreply.github.com>
2020-10-29 08:28:46 -0400

    a

--- index.js
+++ index.js
@@ -2,9 +2,34 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
+const redis = require("redis");
+const client = redis.createClient({ 
+  host : 'redis-19232.c239.us-east-1-2.ec2.cloud.redislabs.com',
+  port : 19232,
+  password : 'mopABt6Djh5nOs4GDTdgBYYUbNxZVG8n'
+});
+
+
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+  client.on("error", function(error) {
+    console.error(error);
+  });
+
+ 
+  client.hgetall("product:87685", function(err, value) {
+    if (err) throw err;
+    console.log(value);
+  });
+
+  function foo(){
+    
+  }
